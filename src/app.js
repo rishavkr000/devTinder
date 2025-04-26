3847776820b72452833871a89169c8bb2d51c92e
Rishav Kumar <rishavkr000@gmail.com>
2025-04-26 12:18:26 +0530

    Exploring different HTTP Methods

--- src/app.js
+++ src/app.js
@@ -2,18 +2,31 @@ const express = require('express');
 
 const app = express();
 
-app.use("/hello", (req, res) => {
-    res.send("Hello Hello Hello!")
+// This will only handle GET requests to the /user route
+app.get("/user", (req, res) => {
+    res.send({firstName: "Rishav", lastName: "Kumar"});
 })
 
-app.use("/test", (req, res) => {
+// This will only handle POST requests to the /user route
+app.post("/user", (req, res) => {
+    res.send("Hello from user post!");
+})
+
+//This will only handle PUT requests to the /user route
+app.put("/user", (req, res) => {
+    res.send("Hello from user put!");
+});
+
+// This will only handle DELETE requests to the /user route
+app.delete("/user", (req, res) => {
+    res.send("Hello from user delete!");
+});
+
+// This will handle all requests to the /user route, regardless of the HTTP method
+app.use("/user", (req, res) => {
     res.send("Hello from test!");
 })
 
-
-app.use((req, res) => {
-    res.send("Hello World!")
-});
 
 app.listen(7777,() => {
     console.log("Server is running on the port 7777");

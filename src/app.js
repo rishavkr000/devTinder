d6493cb3cf34cbb3b11aca24443208d8c6b8f211
Rishav Kumar <rishavkr000@gmail.com>
2025-05-02 15:20:11 +0530

    Wildcard-error-handling

--- src/app.js
+++ src/app.js
@@ -2,17 +2,29 @@ const express = require("express");
 
 const app = express();
 
-const { isAdmin } = require("./middlewares/auth");
+// const { isAdmin } = require("./middlewares/auth");
 
 // app.use("/admin", isAdmin)
 
-app.get("/admin/getAllData", isAdmin, (req, res) => {
-  res.send("Accessed all data");
-});
+// app.get("/getAllData", (req, res) => {
+//   throw new Error("Error occurred while fetching data");
+//   res.send("Accessed all data");
+// });
 
-app.delete("/admin/deleteAllData", (req, res) => {
-  res.send("Deleted all data");
-});
+// app.use("/", (err, req, res, next) => {
+//   res.status(500).send("Something went wrong: " + err.message);
+//   // console.error(err.stack);
+// });
+
+app.get("/getAllData", (req, res) => {
+  try{
+    // Simulating an error
+    throw new Error("Error occurred while fetching data");
+    res.send("Accessed all data");
+  } catch(err) {
+    res.status(500).send("Something went wrong from catch: " + err.message);
+  }
+})
 
 app.listen(7777, () => {
   console.log("Server is running on the port 7777");

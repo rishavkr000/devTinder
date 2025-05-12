62e32d5eac314ff72ab08663d49dc1fd4cbd9468
Rishav Kumar <rishavkr000@gmail.com>
2025-05-12 11:21:48 +0530

    Add Paginition in feed API

--- src/routes/user.js
+++ src/routes/user.js
@@ -60,6 +60,11 @@ userRouter.get("/user/connections", userAuth, async (req, res) => {
 userRouter.get("/user/feed", userAuth, async (req, res) => {
     try {
 
+        const page = parseInt(req.query.page) || 1;
+        let limit = parseInt(req.query.limit) || 10;
+        limit = limit > 50 ? 50 : limit;
+        const skip = (page - 1) * limit;
+
         // User should see all the user card except
         // 1. his own card
         // 2. his connection
@@ -86,7 +91,7 @@ userRouter.get("/user/feed", userAuth, async (req, res) => {
                 { _id: { $nin: Array.from(hideUsersFromFeed) } },
                 { _id: { $ne: loggedInUser._id } }
             ]
-        }).select(userDataToPopulate)
+        }).select(userDataToPopulate).skip(skip).limit(limit);
 
         res.send(users)
 

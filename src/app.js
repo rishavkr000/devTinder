f11ac967df74b5af9629253d9572326db8f95931
Rishav Kumar <rishavkr000@gmail.com>
2025-05-12 16:59:14 +0530

    Install cors package

--- src/app.js
+++ src/app.js
@@ -2,8 +2,10 @@ const express = require("express");
 const connectDB = require("./config/database");
 const app = express();
 const cookieParser = require("cookie-parser");
+const cors = require("cors");
 require("dotenv").config();
 
+app.use(cors());
 app.use(express.json());
 app.use(cookieParser());
 
